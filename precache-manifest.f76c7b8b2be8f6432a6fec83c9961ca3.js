42667ee3d34b1b6b5463433a0c7de06dd7a66b77
SidV <sidv1799@gmail.com>
2019-12-20 00:31:29 -0500

    Updates

---
+++ precache-manifest.f76c7b8b2be8f6432a6fec83c9961ca3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce537f411aa59010eb53ce142d70815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aca4d05d2d92f499389",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e5315e62910b143906f6",
+    "url": "/static/js/2.7d858d3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7d858d3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4aca4d05d2d92f499389",
+    "url": "/static/js/main.6c10a387.chunk.js"
+  },
+  {
+    "revision": "39666f712b41b027ccb8",
+    "url": "/static/js/runtime-main.a72699d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
